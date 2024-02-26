33ca6c392c58c32860923e6669b9235f20679108
chobyong <byongcho@gmail.com>
2024-02-25 22:04:53 -0500

    Update cisco-backup.sh

--- cisco-backup.sh
+++ cisco-backup.sh
@@ -3,4 +3,4 @@
 # source venv/bin/activate
 # python myawesomepythonscript.py
 source /home/ubuntu/ansible-config-backup/cisco/vCisco/bin/activate
-ansible-playbook -i /home/ubuntu/ansible-config-backup/cisco/inventory.ini /home/ubuntu/ansible-config-backup/cisco/cisco-backup.yml
+ansible-playbook -i /home/ubuntu/ansible-config-backup/cisco/inventory.ini /home/ubuntu/ansible-config-backup/cisco/cisco-configbackup.yml
